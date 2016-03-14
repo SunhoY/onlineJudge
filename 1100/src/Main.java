abc50f70be0ee1b1c820d4aae0b5852a3668d2e9
Jade Chang and Sam Lee <pair+jumpitup82+reallhy@gmail.com>
2016-03-14 12:27:34 +0900

    first commit

---
+++ 1100/src/Main.java
@@ -0,0 +1,29 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    private static final int M = 8;
+    private static final String chessman = "F";
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner sc = new Scanner(new File("1100/src/input"));
+//        Scanner sc = new Scanner(System.in);
+        int count = 0;
+
+        for (int i = 0; i < M; i++) {
+            String line = sc.next();
+            for (int j = 0; j < M; j++) {
+                String val = String.valueOf(line.charAt(j));
+
+                boolean isEvenNumber = (i + j) % 2 == 0 ? true : false;
+
+                if(isEvenNumber && chessman.equals(val)) {
+                    count++;
+                }
+            }
+        }
+
+        System.out.println(count);
+    }
+}
