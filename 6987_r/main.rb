e21d46caaf382807fc9a1c9dcc91f3965a857468
Harry Yang and Sam Lee <pair+zillaci+reallhy@gmail.com>
2016-03-24 16:01:02 +0900

    [6971] solving..

--- 6987_r/main.rb
+++ 6987_r/main.rb
@@ -5,7 +5,29 @@ for i in 0..0
   end
 
   while test_case.length != 0
-    selected = test_case.select { |value| value[:win] == test_case.length - 1}
+    max_element = test_case.max_by {|value| value.values.max }
+    max_pair = max_element.max_by {|k, v| v }
+    test_case.delete max_element
+    target_key = '';
+
+    if max_pair[0] == 'win'
+      target_key = 'lose'
+    elsif max_pair[0] == 'lose'
+      target_key = 'win'
+    else
+      target_key = 'draw'
+    end
+
+    to_be_subtracted = max_pair[1];
+
+    for j in 0..test_case.size - 1
+      if test_case[j][target_key] != 0
+        test_case[j][target_key] = test_case[j][target_key] - 1
+      end
+
+
+    end
+
   end
 
 
